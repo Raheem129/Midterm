bb9dcdf99e36ac4b594f5436b7ff2d0f1e2d946a
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-02 19:21:36 -0400

    sample data for users

---
+++ db/seeds/01_users_data.sql
@@ -0,0 +1,13 @@
+
+
+
+INSERT INTO users (name, email, password) VALUES ('Kira Patel', 'kpatel@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Mary Marx', 'mmarx@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Zea Little', 'zlittle@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Roberta Rice', 'rrice@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Chandler Walker', 'cwalker@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Osmond Andrews', 'oandrews@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Tom Potter', 'tpotter@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Conrado Olvera', 'colvera@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Nydia Bush', 'nbush@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Camilla Medina', 'cmedina@gmail.com', 'password');
