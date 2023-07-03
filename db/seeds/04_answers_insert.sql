815d0b42f56ad248778faadc8e1d0be9e8f517ba
Abdi-Raheem <raheem129@hotmail.com>
2023-07-03 10:33:39 -0400

    pulled from remote repo

---
+++ db/seeds/04_answers_insert.sql
@@ -0,0 +1,30 @@
+
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '<h1>', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '<head>', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '<header>', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (1, '<main>', FALSE);
+
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '<a>', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '<link>', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '<href>', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (2, '<hyperlink>', FALSE);
+
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '<ul>', TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, ' <ol>', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '<li>', FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (3, '<list>',FALSE);
+
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, 'src',TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, 'alt',FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, 'href',FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (4, 'img',FALSE);
+
+
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, '<tr>',TRUE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, '<td>',FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, '<table>',FALSE);
+INSERT INTO answers (question_id, answer, isCorrect) VALUES (5, '<th>',FALSE);
