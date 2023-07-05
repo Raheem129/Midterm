b78bf3ce9f34652aa4a285fc7b5bf5ac6a4f33a9
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-05 18:16:03 -0400

    updated file

--- server.js
+++ server.js
@@ -57,9 +57,9 @@ app.use('/quizapp', userRoutes);
 app.get('/', (req, res) => {
   res
     .status(302)
-    .redirect('/quizapp')
+    .redirect('/quizapp');
 });
 
 app.listen(PORT, () => {
-  console.log(`Quizzer App listening on port ${PORT}`);
+  console.log(`Quizzy App listening on port ${PORT}`);
 });
