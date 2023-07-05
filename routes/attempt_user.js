a29213c9213145f34f8d49039168d108e5664157
Aasem Rahuma <aasem.rahuma@gmail.com>
2023-07-05 11:32:39 -0400

    display the users attempt

---
+++ routes/attempt_user.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router  = express.Router();
+
+// attempt page will be displayed with user attempt
+const getAttempt = require ("../db/queries/get_attempts");
+const getUserByID = require ("../db/queries/users");
+
+router.get("/:url",(req, res) => {
+const userId = req.session.userId
+const url = req.params.url
+
+
+promise.all([getUserByID(userId), getAttempt({url}),])
+
+.then(([user, attempt]) => {
+  const templateVars = { attempt, userName: (!user ? '' : user.name) };
+  res.render('quiz_attempt', templateVars);
+})
+
+.catch(error => console.log(error));
+
+});
