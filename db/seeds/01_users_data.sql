6aba1c866f949ede647f373891010ee207dd1ffb
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-05 13:26:27 -0400

    seeds for user data

--- db/seeds/01_users_data.sql
+++ db/seeds/01_users_data.sql
@@ -1,13 +1,5 @@
+INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@quizzes.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Mary', 'mary@quizzes.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Zea', 'zea@quizzes.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Roberta', 'roberta@quizzes.com', 'password');
 
-
-
-INSERT INTO users (name, email, password) VALUES ('Kira Patel', 'kpatel@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Mary Marx', 'mmarx@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Zea Little', 'zlittle@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Roberta Rice', 'rrice@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Chandler Walker', 'cwalker@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Osmond Andrews', 'oandrews@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Tom Potter', 'tpotter@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Conrado Olvera', 'colvera@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Nydia Bush', 'nbush@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Camilla Medina', 'cmedina@gmail.com', 'password');
