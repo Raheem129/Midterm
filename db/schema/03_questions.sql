49c9cc4db441caa0287d9cebcafb2d8cc15586e5
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-05 13:27:37 -0400

    update table

--- db/schema/03_questions.sql
+++ db/schema/03_questions.sql
@@ -4,11 +4,8 @@
 DROP TABLE IF EXISTS questions CASCADE;
 CREATE TABLE questions (
   id SERIAL PRIMARY KEY NOT NULL,
-  quiz_id INTEGER REFERENCES quizzes(id),
-  question TEXT NOT NULL,
-  option1 TEXT NOT NULL,
-  option2 TEXT NOT NULL,
-  option3 TEXT NOT NULL,
-  option4 TEXT NOT NULL,
-  answer TEXT NOT NULL
+  quiz_id INTEGER REFERENCES quizzes(id) ON DELETE CASCADE,
+  text TEXT,
+  sequence INTEGER,
+  UNIQUE(quiz_id, sequence)
 );
