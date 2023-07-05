91563f8a2400e1de5cbc9042376dc092c3be5920
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-05 13:16:39 -0400

    update table

---
+++ db/schema/06_attempt_answers.sql
@@ -0,0 +1,7 @@
+
+DROP TABLE IF EXISTS attempt_answers CASCADE;
+CREATE TABLE attempt_answers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  attempt_id INTEGER REFERENCES attempts(id) ON DELETE CASCADE,
+  answer_id INTEGER REFERENCES answers(id) ON DELETE CASCADE
+);
