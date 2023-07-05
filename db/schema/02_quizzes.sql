77c2245b321faa666a1522a3938425ac204188fc
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-05 13:27:58 -0400

    update table

--- db/schema/02_quizzes.sql
+++ db/schema/02_quizzes.sql
@@ -3,10 +3,11 @@
 DROP TABLE IF EXISTS quizzes CASCADE;
 CREATE TABLE quizzes (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  quiz_type BOOLEAN DEFAULT false,
-  name VARCHAR(255) NOT NULL,
-  description TEXT,
-  category TEXT,
-  modified_date DATE DEFAULT NOW()
+  user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description VARCHAR(255),
+  url VARCHAR(10) UNIQUE,
+  results_url VARCHAR(10) UNIQUE,
+  created_at TIMESTAMP DEFAULT NOW(),
+  is_private BOOLEAN DEFAULT TRUE
 );
