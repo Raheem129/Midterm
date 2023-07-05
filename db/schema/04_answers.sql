cbcd373c9be0ffec7618586d8924e304ea6b0f23
Nimisha Gajjar <nimishagajjar57@gmail.com>
2023-07-05 13:27:20 -0400

    update table

--- db/schema/04_answers.sql
+++ db/schema/04_answers.sql
@@ -3,10 +3,7 @@
 DROP TABLE IF EXISTS answers CASCADE;
 CREATE TABLE answers (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  quiz_id INTEGER REFERENCES quizzes(id),
-  question_id INTEGER REFERENCES questions(id),
-  result_id INTEGER REFERENCES results(id),
-  user_answer TEXT NOT NULL,
-  test_date DATE DEFAULT NOW()
+  question_id INTEGER REFERENCES questions(id) ON DELETE CASCADE,
+  text TEXT,
+  is_correct BOOLEAN DEFAULT FALSE
 );
